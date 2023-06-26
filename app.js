f5f10f2710b9d1a270d1e50c06d8455386a3de91
navyavenkat <navyavenkat03@gmail.com>
2023-06-25 21:58:17 -0700

    Connect the Mongo db

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express=require('express')
-const app=express()
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = 3500
+const patientsRouter = require('./routes/patientsRouter')
+// const validatePatientsRouter = require('./routes/validatePatientsRouter')
+const validatePatientsRouter=require('./routes/validatePatientsRouter')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.get('/',(req,res)=>{
-    res.send("server")
-})
+app.use(cors())
+app.use(express.json())
 
-app.listen(3500)
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (errorMessage) => console.log(errorMessage))
+db.once('open', () => console.log('Connected successfully to the database..'))
+
+app.use('/api/v1/patients', patientsRouter)
+app.use('/api/v1/patients/validate', validatePatientsRouter)
+
+app.listen(PORT, console.log(`Server listening at http://localhost:${PORT}/api/v1/patients`))
